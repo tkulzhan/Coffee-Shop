da158d1a37d269aa2ccfeeb6310c17fdb58d1ed5
PolozovMichael <polozovmisha.sm.nis.edu.kz@gmail.com>
2023-04-25 21:47:28 +0600

    feat: basic project configuration

--- app/index.js
+++ app/index.js
@@ -1,11 +1,25 @@
-import { View, Text } from "react-native";
+import { Stack } from "expo-router";
+import { View, Text, SafeAreaView } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
 function Home() {
   return (
-    <View>
-      <Text>Home page</Text>
-    </View>
-  );
+    <SafeAreaView style = {{flex: 1}}>
+      <Stack.Screen options=
+      {{
+        headerTitle: "Coffee Base", 
+        headerShadowVisible: false
+      }} />
+      <ScrollView style = {{
+          backgroundColor: "#9D00AB",
+          height: "100%"
+         }} showsVerticalScrollIndicator = {false}>
+         <View>
+
+         </View>
+      </ScrollView>
+    </SafeAreaView>
+  ); 
 }
 
 export default Home;
---
+++ components/mainpage/ComunityCard.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native"
+
+function ComunityCard() {
+    return (
+        <View style>
+
+        </View>
+    )
+}
+
+export default ComunityCard;
